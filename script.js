13804c4edb588f1fbe01bc1c1123c62ee87572ab
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-20 12:57:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function insert_Row() {
 let table=document.getElementById('sampleTable');
 	let row=table.insertRow(0);
-	let cell1=table.insertCell(0);
-	let cell2=table.insertCell(1);
+	let cell1=row.insertCell(0);
+	let cell2=row.insertCell(1);
 	cell1.textContent="New Cell1";
 	cell2.textContent="New Cell2";
 }
