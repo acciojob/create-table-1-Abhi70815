63ff94a8919f9984324ffb9990b44bec500aa5c9
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-20 11:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function insert_Row() {
     //Write your code here
-  document.getElementById('sampleTable').innerHTML=`<tr><td>NewCell1</td> 
+  let sample=document.getElementById('sampleTable')
+	  sample.innerHTML=`<tr><td>NewCell1</td> 
 		<td>NewCell2</td></tr> <tr><td>Row1 cell1</td> 
 		<td>Row1 cell2</td></tr> 
 		<tr><td>Row2 cell1</td> 
 		<td>Row2 cell2</td></tr>`
+	
   
 }
